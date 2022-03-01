a3a101edb288d92c6073adb95cb7dec47e8c6674
Francis Fang <frank_fang2011@live.cn>
2022-03-01 13:45:38 -0500

    Finished LearYear.java in Lab01

--- lab01/LeapYear.java
+++ lab01/LeapYear.java
@@ -10,6 +10,14 @@ public class LeapYear {
      */
     public static boolean isLeapYear(int year) {
         // TOOD: Fill in this method.
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else if (year % 4 == 0) {
+            return true;
+        }
+        return false;
     }
 
     /** Calls isLeapYear to print correct statement. */
